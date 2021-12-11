c6190e3ab2f185768dea1b945f3104b8cb35e1fe
RafaelRibeiroEstrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2021-12-11 11:53:29 -0400

    update

--- Implementation/Backend/api-GerarSenhaAtendimento/src/main/java/com/example/gerarsenhaatendimento/repositories/GuicheRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gerarsenhaatendimento.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.gerarsenhaatendimento.models.Guiche;
-
-public interface GuicheRepository extends JpaRepository<Guiche, Long>{
-
-}
